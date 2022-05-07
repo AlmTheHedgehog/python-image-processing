b7ee089b5a1272e0062699cc722de3bc07cfbf38
AlmTheHedgehog <98266372+AlmTheHedgehog@users.noreply.github.com>
2022-05-07 21:52:04 +0200

    Upload

---
+++ histogram_equalization.py
@@ -0,0 +1,34 @@
+from PIL import Image
+
+if __name__ == '__main__':
+    im_yoda = Image.open("AandDS/2lab/yoda.jpeg")
+    yoda_arr = list(im_yoda.getdata())
+
+
+    yoda_arr_gray = [[0]*3] * (im_yoda.height*im_yoda.width)
+    number_of_pixels = [0] * (im_yoda.height*im_yoda.width)
+    pdf = [0] * (im_yoda.height*im_yoda.width)
+    cdf = [0] * (im_yoda.height*im_yoda.width)
+    yoda_arr_eq_changes = [0] * (im_yoda.height*im_yoda.width)
+    #to grayscale and count number of pixels per level
+    for i in range(len(yoda_arr_gray)):
+        yoda_arr_gray[i] = [int(yoda_arr[i][0]*0.299 + yoda_arr[i][1]*0.587 + yoda_arr[i][2]*0.114)] * 3
+        number_of_pixels[yoda_arr_gray[i][0]] += 1
+
+    #pdf and cdf calc + eq value 
+    for i in range(len(yoda_arr_gray)):
+        pdf[i] = number_of_pixels[i] / (im_yoda.height*im_yoda.width)
+        if i == 0:
+            cdf[i] = pdf[i]
+        else:
+            cdf[i] = cdf[i-1] + pdf[i]
+        yoda_arr_eq_changes[i] = round(cdf[i] * 256)
+
+    #changing input pixels values to eq
+    for i in range(len(yoda_arr_gray)):
+        yoda_arr_gray[i] = tuple([yoda_arr_eq_changes[yoda_arr_gray[i][0]]] * 3)
+
+
+    im_yoda_eq = Image.new(im_yoda.mode, im_yoda.size)
+    im_yoda_eq.putdata(yoda_arr_gray)
+    im_yoda_eq.save("AandDS/2lab/yoda_eq.jpg")
---
+++ rgb_to_bin_im(single, double threshold).py
@@ -0,0 +1,43 @@
+from PIL import Image
+
+if __name__ == '__main__':
+    im_yoda = Image.open("AandDS/2lab/yoda.jpeg")
+    yoda_arr = list(im_yoda.getdata())
+
+    answ = "3"
+    while (answ != "1") and (answ != "2"):
+        answ = input("Single TH - 1; double - 2:")
+        if answ == "1":
+            is_singleTH = True
+        elif answ == "2":
+            is_singleTH = False
+        else:
+            print("Eroor, try one more")
+
+    answ = 256
+    while not (0 <= answ <= 255):
+        answ = int(input("Enter first threshold(0 <= answer <= 255):"))
+    threshold = [answ]
+    if not is_singleTH:
+        answ = 256
+        while not (0 <= answ <= 255):
+            answ = int(input("Enter second threshold(0 <= answer <= 255):"))
+        threshold.append(answ)
+
+    #to grayscale and binary
+    yoda_arr_bin = [[0]*3] * (im_yoda.height*im_yoda.width)
+    for i in range(len(yoda_arr_bin)):
+        if (yoda_arr[i][0]*0.299 + yoda_arr[i][1]*0.587 + yoda_arr[i][2]*0.114) >= threshold[0]:
+            if not is_singleTH:
+                if (yoda_arr[i][0]*0.299 + yoda_arr[i][1]*0.587 + yoda_arr[i][2]*0.114) <= threshold[1]:
+                    yoda_arr_bin[i] = tuple([255, 255, 255])
+                else:
+                    yoda_arr_bin[i] = tuple([0, 0, 0])
+            else:
+                yoda_arr_bin[i] = tuple([255, 255, 255])
+        else:
+            yoda_arr_bin[i] = tuple([0, 0, 0])
+
+    im_yoda_binary = Image.new(im_yoda.mode, im_yoda.size)
+    im_yoda_binary.putdata(yoda_arr_bin)
+    im_yoda_binary.save("AandDS/2lab/yoda_binary.jpg")
